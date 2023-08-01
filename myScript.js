c17f499f88f0b1c456542cf976573406384f5991
emahnaf <emahnaf@gmail.com>
2023-08-01 15:44:29 +0800

    ini adalah push pertama yg saya lakukan dari vsc

--- myScript.js
+++ myScript.js
@@ -26,3 +26,72 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+// const navLinks = document.querySelectorAll('nav a');
+
+//   navLinks.forEach(link => {
+//     link.addEventListener('click', (event) => {
+//       event.preventDefault();
+//       const targetId = link.dataset.target;
+//       const targetSection = document.getElementById(targetId);
+      
+//       // Scroll to the target section smoothly
+//       targetSection.scrollIntoView({ behavior: 'smooth' });
+//     });
+//   });
+
+// $("a").click(function(event){
+//   event.preventDefault();
+// });
+
+// Get the button
+// let mybutton = document.getElementById("tombolbtt");
+
+// INI UNTUK BTT<<<<<<<<<<<
+// Get the button
+let mybutton = document.getElementById("tombolbtt");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+// INI UNTUK SCRIPTNYAA BUKA MODAL DI IG
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("ig");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
